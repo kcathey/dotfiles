cb66c2a3452a52f7ffa4a8a331fa6c736a391e3e
Kevin C. Athey <drizztdn@gmail.com>
2026-05-29 23:11:45 -0700

    added code alias for darwin

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,3 +1,8 @@
 alias reload!='. ~/.bashrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+if [ "$(uname -s)" == "Darwin" ]
+then
+  alias code='/Applications/Visual\ Studio\ Code.app/Contents/MacOS/Code'
+fi
